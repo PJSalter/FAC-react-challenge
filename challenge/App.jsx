80dc8a9e22a51073bfe163b0ee37cd5800320e9d
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-10-17 16:36:54 +0100

    passed in shouter component inside app

--- challenge/App.jsx
+++ challenge/App.jsx
@@ -1,9 +1,11 @@
 import Greeting from "../challenge/Greeting";
+import Shouter from "./Shouter";
 
 function App() {
   return (
     <main>
       <Greeting name="oli" />
+      <Shouter />
     </main>
   );
 }
